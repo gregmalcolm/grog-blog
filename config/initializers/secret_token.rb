dcca62216f49092a046d33dcbd3dc3599885584f
gregmalcolm <greg@gregmalcolm.com>
2013-05-09 19:41:54 +0100

    Created new rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GrogBlog::Application.config.secret_key_base = 'debdd5e5558a07d22ee3b400314e123f3f541570b75d1c82e885d21ff4468c11df8a2de87a666540404705ad0352d9f3764430c135215730a74b03787e12ed79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GrogBlog::Application.config.session_store :cookie_store, key: '_grog-blog_session'

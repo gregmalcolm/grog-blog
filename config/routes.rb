0a9b739c900b7e9b7ff29ca3e4b3dc10ce4a68da
gregmalcolm <greg@gregmalcolm.com>
2013-05-14 17:01:04 +0100

    Tweaked devise to suit my needs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 GrogBlog::Application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'admin', to: "devise/sessions#new"
+    get 'logout', to: "devise/sessions#destroy"
+    get 'signout', to: "devise/sessions#destroy"
+  end
+
   resources :posts
 
   root 'posts#index'
